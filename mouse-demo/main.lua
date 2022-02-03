191407324c1a7670d1212b62f362c16703392129
Jen Guerra <jen@southpawgeek.com>
2022-02-02 19:43:06 -0500

    Mouse Demo

---
+++ mouse-demo/main.lua
@@ -0,0 +1,47 @@
+function love.load()
+    mousefocus = "mousefocus - n/a"
+    mousemoved = "mousemoved - n/a"
+    mousemovedd = "mousemoved - n/a"
+    mousepressed = "mousepressed - n/a"
+    mousereleased = "mousereleased - n/a"
+    --isgrabbed = "grabbed - n/a"
+end
+
+function love.update(dt)
+--[[     grabbed = love.mouse.isGrabbed()
+    if grabbed then
+        isgrabbed = "grabbed - true"
+    else
+        isgrabbed = "grabbed - false"
+    end ]]
+end
+
+function love.draw()
+    love.graphics.print(mousefocus, 10, 10)
+    love.graphics.print(mousemoved, 10, 25)
+    love.graphics.print(mousemovedd, 10, 40)
+    love.graphics.print(mousepressed, 10, 55)
+    love.graphics.print(mousereleased, 10, 70)
+    --love.graphics.print(isgrabbed, 10, 85)
+end
+
+function love.mousemoved(x, y, dx, dy, istouch)
+    mousemoved = "mousemoved - x: " .. x .. " y: " .. y
+    mousemovedd = "mousemoved - dx: " .. dx .. " dy: " .. dy
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+    mousepressed = "mousepressed - x: " .. x .. " y: " .. y .. " button: " .. button .. " presses: " .. presses
+end
+
+function love.mousereleased(x, y, button, istouch, presses)
+    mousereleased = "mousereleased - x: " .. x .. " y: " .. y .. " button: " .. button .. " presses: " .. presses
+end
+
+function love.mousefocus(f)
+    if f then
+        mousefocus = "mousefocus - hoem"
+    else
+        mousefocus = "mousefocus - not hoem"
+    end
+end
