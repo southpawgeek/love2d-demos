e0c769cc0bd0931fd78dc95c9f6a7eb1ec11b4b2
Jen Guerra <southpawgeek@users.noreply.github.com>
2022-07-26 17:34:37 -0400

    skeleton get

---
+++ skeleton/main.lua
@@ -0,0 +1,69 @@
+-- load some third-party libraries and initialize some other stuff
+require 'src/setup'
+
+-- runs once on application start
+function love.load()
+    -- without this, randomized stuff will not be random
+    math.randomseed(os.time())
+    -- set window title
+    love.window.setTitle(TITLE)
+    -- this keeps the pixels looking pixelly
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    -- Push does some stuff
+    Push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = true,
+        vsync = true
+    })
+
+    -- get refresh rate so our dt/NDT stuff is nice
+    getRefresh()
+
+    -- initialize keys and mouse button tables
+    love.keyboard.keysPressed = {}
+    love.mouse.buttonsPressed = {}
+
+    -- start by loading the Title Screen
+    Screen:change('Title')
+end
+
+-- this is the game loop, dt is usually a fraction of a second
+function love.update(dt)
+    -- keep a running total of time passed
+    NDT = NDT + dt
+
+    -- only update once per.. hz?
+    if NDT > 1/REFRESH then
+        Screen:update(NDT)
+        -- if NDT is over 1 second, reset it to the remainder
+        NDT = NDT % 1/REFRESH
+
+        -- refresh these tables because they were handled by the Screen:update method
+        love.keyboard.keysPressed = {}
+        love.mouse.buttonsPressed = {}
+    end
+end
+
+-- renders graphics, probably don't need to touch this
+function love.draw()
+    Push:start()
+    Screen:render()
+    Push:finish()
+end
+
+-- Push thing, can leave this alone
+function love.resize(w, h)
+    Push:resize(w, h)
+    getRefresh()
+end
+
+-- sets a bool so we know what key is pressed
+function love.keypressed(key)
+    love.keyboard.keysPressed[key] = true
+end
+
+-- returns a bool of whether the key was pressed
+function love.keyboard.wasPressed(key)
+    return love.keyboard.keysPressed[key]
+end
