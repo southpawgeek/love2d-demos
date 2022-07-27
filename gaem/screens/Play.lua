3fcb27c6f352427fcd9760553e9d4477cc34c5a9
Jen Guerra <southpawgeek@users.noreply.github.com>
2022-07-27 00:10:07 -0400

    particles?

---
+++ gaem/screens/Play.lua
@@ -0,0 +1,56 @@
+Play = Class{__includes = BaseState}
+Play._name = 'Play'
+
+function Play:enter()
+    SCREEN = self._name
+end
+
+function Play:render()
+    Player:render()
+
+    for k, item in pairs(Projectiles) do
+        item:render()
+    end
+end
+
+function Play:update(dt)
+    -- esc to go back to title
+    if love.keyboard.wasPressed('escape') then
+        Screen:change('Title')
+    end
+
+    -- arrow controls
+    if love.keyboard.isDown('up') then
+        Player.dy = Player.dy - 1
+    end
+
+    if love.keyboard.isDown('down') then
+        Player.dy = Player.dy + 1
+    end
+
+    if love.keyboard.isDown('left') then
+        Player.dx = Player.dx - 1
+    end
+
+    if love.keyboard.isDown('right') then
+        Player.dx = Player.dx + 1
+    end
+
+    if love.keyboard.isDown('space') then
+        Player:shoot()
+    end
+
+    Player:update(dt)
+
+    -- update projectiles
+    for k, item in pairs(Projectiles) do
+        item:update(dt)
+    end
+
+    -- remove dead projectiles
+    for k, item in pairs(Projectiles) do
+        if item.dead then
+            table.remove(Projectiles, k)
+        end
+    end
+end
---
+++ gaem/src/setup.lua
@@ -0,0 +1,55 @@
+-- use third-party stuff
+Class = require 'lib/class'
+Push = require 'lib/push'
+
+require 'lib/BaseState'
+require 'lib/StateMachine'
+
+-- game objects
+require 'obj/Player'
+require 'obj/Projectile'
+
+-- screen states
+require 'screens/Title'
+require 'screens/Play'
+
+-- Push uses this to create the window
+WINDOW_WIDTH = 1024
+WINDOW_HEIGHT = 576
+
+-- normalized delta time
+NDT = 0
+
+-- current screen state, note the states in the screens directory update this
+SCREEN = ''
+
+-- refresh rate, this can change if you fullscreen or drag to different monitors
+REFRESH = 0
+
+-- (probably) constant values
+-- window title
+TITLE = 'cool game title'
+
+-- the game gets scaled to this resolution
+VIRTUAL_WIDTH = 256
+VIRTUAL_HEIGHT = 144
+
+-- mouse button constants
+PRIMARY_BUTTON = 1 -- usually left-click unless OS settings alter it
+SECONDARY_BUTTON = 2 -- usually right-click
+THIRD_BUTTON = 3 -- usually middle-click
+
+-- utilities
+-- sets the refresh rate global, defaults to 60
+function getRefresh()
+    undef, undef, flags = love.window.getMode()
+    REFRESH = flags.refreshrate
+    if REFRESH == 0 then REFRESH = 60 end
+    print('setting refresh rate to ' .. REFRESH)
+end
+
+-- state machine for screens
+Screen = StateMachine {
+    ['Title'] = function() return Title() end,
+    ['Play'] = function() return Play() end
+}
